70ed719c279c8e7db84b786bc5d1ebe355088ab6
Tom <huang420@students.cs.ubc.ca>
2023-11-20 17:18:49 -0800

    second commit

--- script.py
+++ script.py
@@ -1,8 +1,16 @@
 import requests
 from collections import defaultdict
+from flask import Flask, redirect, request
+from dotenv import load_dotenv
+import secrets
+import os
 
-client_id = '4bc9917a9eb24f6bb9d2061034aa5682'
-client_secret = 'f0ebcd8f19b24fc9aba8cd731a8d9836'
+app = Flask(__name__)
+load_dotenv()
+
+client_id = os.getenv('SPOTIFY_CLIENT_ID')
+client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+redirect_uri = 'http://localhost:8888/callback'
 access_token = None
 playlists_I_want = set(['Demon', '3am', 'DY', 'Does it get better than this', 'K', '青春', 'Piano x2', 'Artcore x2', '日本語'])
 
@@ -29,8 +37,6 @@ def get_access_token(client_id, client_secret):
     
     return access_token
 
-access_token = get_access_token(client_id, client_secret)
-
 def get_playlists():
     url = "https://api.spotify.com/v1/users/12179241495/playlists"
     headers = {
@@ -46,9 +52,6 @@ def get_playlists():
         data = str(response.status_code) + response.text
     
     return data
-
-playlists = get_playlists()['items']
-playlist_name_id= [(playlist['name'], playlist['id']) for playlist in playlists if playlist['name'] in playlists_I_want ]
 
 def get_playlist_songs(playlist_id):
     url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
@@ -69,26 +72,47 @@ def get_playlist_songs(playlist_id):
     
     return tracks_items
 
-good_playlist = defaultdict(list) # playlist_name : [(artists, song name)]
 
-for playlist_name, playlist_id in playlist_name_id: 
-    songs = get_playlist_songs(playlist_id)
-    
-    if songs:
-        for song in songs:
-        
-            song_artists = song['track'].get('artists', None)
-            song_name = song['track'].get('name', None)
-            
-            if song_artists and song_name:
-                all_artists_string = ', '.join([artist.get('name', '') for artist in song_artists])
-                good_playlist[playlist_name].append((all_artists_string, song_name))
-               
-            elif not song_artists and not song_name:
-                print('This song cannot be added')
+@app.route('/')
+def index():
+    return redirect("/login")
 
-print(good_playlist)
+@app.route('/login')
+def login():
+    state = secrets.token_urlsafe(16)
+      
+    # Spotify authorization URL
+    auth_url = "https://accounts.spotify.com/authorize?" + \
+               f"client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&state={state}&scope=playlist-read-private"
+    return redirect(auth_url)
 
+@app.route('/callback')
+def callback():
+    # Handle the callback from Spotify
+    # Extract the authorization code from the request
+    code = request.args.get('code')
+    # Exchange code for access token
+    access_token = get_access_token(client_id, client_secret)
+    # Fetch playlists
+    playlists = get_playlists(access_token)['items']
+    playlist_name_id = [(playlist['name'], playlist['id']) for playlist in playlists if playlist['name'] in playlists_I_want]
+    # Fetch songs from playlists
+    good_playlist = defaultdict(list)
+    for playlist_name, playlist_id in playlist_name_id: 
+        songs = get_playlist_songs(access_token, playlist_id)
+        if songs:
+            for song in songs:
+                song_artists = song['track'].get('artists', None)
+                song_name = song['track'].get('name', None)
+                if song_artists and song_name:
+                    all_artists_string = ', '.join([artist.get('name', '') for artist in song_artists])
+                    good_playlist[playlist_name].append((all_artists_string, song_name))
+    return str(good_playlist)  # Convert the result to string for display
+
+
+
+if __name__ == '__main__':
+    app.run(port=8888)
       
 
 
